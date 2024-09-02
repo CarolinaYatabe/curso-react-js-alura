7ba59a7559470239dcb67724f7e88b117f504c65
Carolina Ayumi Yatabe <carolina.yatabe@itlab.com.br>
2024-09-02 15:47:29 -0300

    adicionando o prevent default no submit do botão

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -13,7 +13,7 @@ const CampoTexto = (props) => {
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}></input>
+            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
         </div>
     )
 }
