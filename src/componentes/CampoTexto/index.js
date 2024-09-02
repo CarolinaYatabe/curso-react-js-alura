ea981539f7eac78b26cdffb9f4e19103ba6ebe69
Carolina Ayumi Yatabe <carolina.yatabe@itlab.com.br>
2024-09-02 15:17:05 -0300

    Adicionando o formulário

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,21 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+    /*
+    Para fazer interpolação de strings poderiamos fazer assim:
+    const placeholderModificada = `${props.placeholder}`
+    e o campo imput ficaria assim:
+    <input placeholder={placeholdermodificada}></input>
+
+    */
+
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input placeholder={props.placeholder}></input>
+        </div>
+    )
+}
+
+export default CampoTexto
